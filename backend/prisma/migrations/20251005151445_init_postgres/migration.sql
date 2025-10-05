a9c79bd31dc0622326e83d2bcde6f7336f35390c
purubhoite <psbedits26@gmail.com>
2025-10-05 20:45:57 +0530

    Feat: create postgres migrations

--- backend/prisma/migrations/20251005151445_init_postgres/migration.sql
+++ backend/prisma/migrations/20251005151445_init_postgres/migration.sql
@@ -1,29 +1,35 @@
 -- CreateTable
 CREATE TABLE "Check" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "originalClaim" TEXT NOT NULL,
     "cleanedClaim" TEXT NOT NULL,
     "verdict" TEXT NOT NULL,
     "confidence" INTEGER NOT NULL,
     "summary" TEXT NOT NULL,
-    "checkedAt" DATETIME NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "checkedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Check_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Source" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "source" TEXT NOT NULL,
     "url" TEXT NOT NULL,
     "publishedAt" TEXT NOT NULL,
     "snippet" TEXT NOT NULL,
     "reliabilityScore" INTEGER NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "checkId" TEXT,
-    CONSTRAINT "Source_checkId_fkey" FOREIGN KEY ("checkId") REFERENCES "Check" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+    CONSTRAINT "Source_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Source_url_key" ON "Source"("url");
+
+-- AddForeignKey
+ALTER TABLE "Source" ADD CONSTRAINT "Source_checkId_fkey" FOREIGN KEY ("checkId") REFERENCES "Check"("id") ON DELETE SET NULL ON UPDATE CASCADE;
